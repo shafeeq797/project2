106ff403994d5814a50559b75fd9a8ca2a05782b
Arathi <pythontrainer.tecswan@gmail.com>
2023-08-18 10:59:36 +0530

    link connection

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
-
+def link(request):
+    return render(request,'link.html')
 # Create your views here.
--- work/urls.py
+++ work/urls.py
@@ -21,4 +21,5 @@ from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
+    path('link/',views.link),
 ]
